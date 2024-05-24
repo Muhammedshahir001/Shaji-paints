9fc789af7f8105552fb1798e6dfd5d3548633176
Muhammedshahir001 <muhammedshahir91@gmail.com>
2024-05-24 16:18:58 +0530

    new logo added

--- src/components/user/Layouts/Header.jsx
+++ src/components/user/Layouts/Header.jsx
@@ -17,11 +17,15 @@ function Header() {
     >
       <div className="hidden lg:block">
         <div className=" flex gap-10 justify-between  p-5">
-          <a
-            href="#"
-            className="flex items-center space-x-3 rtl:space-x-reverse ml-[5%]"
-          >
-            <img src={logo} className="h-8" alt="Flowbite Logo" />
+          <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
+            <img
+              src="https://flowbite.com/docs/images/logo.svg"
+              class="h-8"
+              alt="Flowbite Logo"
+            />
+            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              Flowbite
+            </span>
           </a>
 
           <div
