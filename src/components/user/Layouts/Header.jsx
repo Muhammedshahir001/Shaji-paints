57e3dfb5cc3b48545db4eb3c5f0ca9ea9a3a66d8
Muhammedshahir001 <muhammedshahir91@gmail.com>
2024-05-24 15:58:06 +0530

    new navbar

--- src/components/user/Layouts/Header.jsx
+++ src/components/user/Layouts/Header.jsx
@@ -14,11 +14,7 @@ function Header() {
     <nav className="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
-          <img
-            src="https://flowbite.com/docs/images/logo.svg"
-            className="h-8"
-            alt="Flowbite Logo"
-          />
+          <img src={logo} className="h-8" alt="Flowbite Logo" />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
             Flowbite
           </span>
